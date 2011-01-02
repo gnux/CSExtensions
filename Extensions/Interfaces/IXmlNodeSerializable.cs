1e58623ac563008c33f56e65a401564491bf06cc
Georg Neubauer <georg.neubauer@tugraz.at>
2011-01-02 23:34:42 +0100

    added IXmlNodeSerializable

---
+++ Extensions/Interfaces/IXmlNodeSerializable.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml;
+
+namespace gnux.Extensions.Interfaces
+{
+    public interface IXmlNodeSerializable
+    {
+        string XML_NODE_NAME { get; }
+
+        /// <summary>
+        /// Write object to a XML Node
+        /// </summary>
+        /// <param name="node"></param>
+        void Write(XmlNode node);
+
+        /// <summary>
+        /// Read object from a XML node
+        /// </summary>
+        /// <param name="node"></param>
+        void Read(XmlNode node);
+    }
+}
